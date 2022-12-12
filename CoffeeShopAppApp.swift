97982971b74ef4ef45a6042e7e0926e834f0016b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-11 19:32:01 -0600

    Create CoffeeShopAppApp.swift

---
+++ CoffeeShopAppApp.swift
@@ -0,0 +1,27 @@
+//
+//  CoffeeShopAppApp.swift
+//  CoffeeShopApp
+//
+//  Created by Jayce Sagvold on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                ContentView()
+                    .tabItem {
+                        Label("Cofees ", systemImage: "mug.fill")
+                    }
+                SkipTheLine()
+                    .tabItem {
+                        Label("Skip the Line", systemImage: "arrow.triangle.swap")
+                        
+                    }
+            }
+        }
+    }
+}
