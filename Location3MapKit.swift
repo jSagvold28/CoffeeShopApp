2f1e0cf88baeb9818755018f157aa932b0a9d6f9
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-12 19:11:20 -0600

    Create Location3MapKit.swift

---
+++ Location3MapKit.swift
@@ -0,0 +1,44 @@
+//
+//  Location2MapKit.swift
+//  CoffeeShopApp
+//
+//  Created by Jayce Sagvold on 12/12/22.
+//
+
+import SwiftUI
+
+import MapKit
+
+
+
+struct Location2MapKit: View {
+
+    @State private var region = MKCoordinateRegion(
+
+        center: CLLocationCoordinate2D(latitude: 34.0522, longitude: 118.2437),
+
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+
+    )
+
+
+
+    var body: some View {
+
+        Map(coordinateRegion: $region)
+
+    }
+
+}
+
+
+
+struct Location2MapKit_Previews: PreviewProvider {
+
+    static var previews: some View {
+
+        Location2MapKit()
+
+    }
+
+}
